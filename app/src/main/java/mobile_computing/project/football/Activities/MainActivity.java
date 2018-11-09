0f24ca57be0a612e019c681e22da2192c6200909
ajkh35 <ajay35@live.in>
2018-11-09 12:17:57 +0100

    Release changes.

--- app/src/main/java/mobile_computing/project/football/Activities/MainActivity.java
+++ app/src/main/java/mobile_computing/project/football/Activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -15,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 import org.json.simple.*;
@@ -274,6 +276,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this,R.string.no_settings,Toast.LENGTH_SHORT).show();
             return true;
         }
 
